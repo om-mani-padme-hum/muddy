bd4c08b2e7c81ae2ae7eb88f66eda933393d46fc
Rich Lowe <rich.fb.email@gmail.com>
2021-09-04 09:41:53 -0500

    Updated SQL for creating database to match latest code version

--- muddy.sql
+++ muddy.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `muddy` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `muddy`;
 -- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: muddy
@@ -367,4 +369,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-16 20:06:43
+-- Dump completed on 2021-09-04  9:39:27
